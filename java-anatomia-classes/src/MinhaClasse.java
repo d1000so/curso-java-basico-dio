3e4e9aeef88b6a7e14e279357ffa12699d56f1c0
d1000so <d1000so@unicamp.br>
2023-09-03 13:42:52 -0300

    inclusão do projeto anatomia classes

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,17 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        
+        String primeiroNome = "Edmilson";
+        String segundoNome = "Braga";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+        
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+
+        
+
+    }
+}
--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas Vindas a galera da DIO!");
+        int num;
+        num = 6 * 5;
+        System.out.println("Boas Vindas a galera da DIO!" + num);
     }
 }
