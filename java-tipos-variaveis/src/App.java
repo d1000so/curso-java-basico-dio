3196b25b6a717f00592264609a04c13b6ee48f5c
d1000so <d1000so@unicamp.br>
2023-09-08 19:19:26 -0300

    adicionando conteudo ao projeto condicionais

--- java-tipos-variaveis/src/App.java
+++ java-tipos-variaveis/src/App.java
@@ -1,5 +1,17 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // ComparacaoAvancada.java
+        int numero1 = 2;
+        int numero2 = 1;
+
+        if(numero1== 2 && ++ numero2 == 2 )
+            System.out.println("As 2 condições são verdadeiras");
+
+        System.out.println("O numero 1 agora é " + numero1);
+        System.out.println("O numero 2 agora é " + numero2);
+
+        // Vamos mudar a linha 5 do código acima para:if(numero1== 2 && numero2 ++ == 2 )
+
+
     }
 }
