376dc37a94c08a26786c28393464f450373af80d
d1000so <d1000so@unicamp.br>
2023-09-09 19:06:00 -0300

    inclui projeto smarttv

---
+++ java-smart-tv/src/SmartTv.java
@@ -0,0 +1,11 @@
+public class SmartTv {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+/*Vamos criar um exemplo de uma classe para representar uma SmartTV onde:
+Ela tenha as características: ligada (boolean), canal (int) e volume (int);
+Nossa TV poderá ligar e desligar e assim mudar o estado ligada;
+Nossa TV aumentará e diminuirá o volume sempre em +1 ou -1;
+Nossa TV poderá mudar de canal de 1 em 1 ou definindo o número correspondente.*/
