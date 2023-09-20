8d1aa4eabe7d9652677de9eb9f726b2c6b1957b6
d1000so <d1000so@unicamp.br>
2023-09-19 23:42:29 -0300

    finished class exception

--- java-tratamento-excecoes/src/AboutMe.java
+++ java-tratamento-excecoes/src/AboutMe.java
@@ -1,8 +1,32 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        try {
+            System.out.println("Digite seu nome:");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome:");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade:");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura:");
+
+            double altura = scanner.nextDouble();
+        }
+        catch(InputMismatchException e) {
+            System.err.println("Digite apenas valores numéricos para idade e altura");
+        }
+
+
+        scanner.close();
+
     }
+    
 }
